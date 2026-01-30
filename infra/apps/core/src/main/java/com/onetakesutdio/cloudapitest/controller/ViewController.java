1483208edd3597d1ead8d299965e6bc25750d55f
김영웅 <duddnd4457@naver.com>
2026-01-31 01:05:10 +0900

    feat: controller path 변경 (/ -> /test-api)

--- infra/apps/core/src/main/java/com/onetakesutdio/cloudapitest/controller/ViewController.java
+++ infra/apps/core/src/main/java/com/onetakesutdio/cloudapitest/controller/ViewController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ViewController {
 
-    @GetMapping("/")
+    @GetMapping("/test-api")
     public String index() {
         return "index";
     }
